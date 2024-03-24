af9b76e3c2984e2d5d9142e43776b1c78914fea6
stephenmunyi <stephenmunyi075@gmail.com>
2024-03-24 10:34:49 +0300

    complete task

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random;
+do {
+    random = Math.floor(Math.random() *100)
+} while (random=== 0)
+
+let num3 = 20
+let num4 = 16
+let mod = num3 % num4
+
+let max = Math.max(num3, num4)
+let highest = num3
